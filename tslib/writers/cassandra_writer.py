2ea6cf5367ea2eb5092bddc8e006f6104c9b5a4e
Berto Booijink <berto.booijink@nelen-schuurmans.nl>
2012-10-11 16:45:58 +0200

    Cassandra writer now uses time buckets

--- tslib/writers/cassandra_writer.py
+++ tslib/writers/cassandra_writer.py
@@ -1,17 +1,25 @@
+import pycassa
+import json
+
+
 class CassandraWriter(object):
 
-    def __init__(self, cf):
-        self.cf = cf
+    def __init__(self, nodes, keyspace, column_family, queue_size):
+        pool = pycassa.ConnectionPool(keyspace=keyspace, server_list=nodes,
+            prefill=False)
+        self.cf = pycassa.ColumnFamily(pool, column_family)
+        self.qs = queue_size
 
-    def write(self, dfs):
-        with self.cf.batch() as b:
-            i = 0
-            for df in dfs:
-                i += 1
-                key = 'sensor-%d-' % i
-                key = key + '%Y-%m-%dT%H:%M:%S%z'
+    def write(self, dataframes):
+        sensor_id = 'pixml-test'
+        key_format = sensor_id + ':%Y-%m'
+        colname_format = '%Y-%m-%dT%H:%M:%S%z'
+        with self.cf.batch(queue_size=self.qs) as b:
+            for df in dataframes:
+                print json.dumps(df)
                 for timestamp, row in df.iterrows():
                     b.insert(
-                        timestamp.strftime(key),
-                        dict((k, str(v)) for k, v in row.to_dict().iteritems())
+                        timestamp.strftime(key_format),
+                        dict(("%s_%s" % (timestamp.strftime(colname_format), k),
+                              str(v)) for k, v in row.to_dict().iteritems())
                     )

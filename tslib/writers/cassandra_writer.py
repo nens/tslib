5f486a5b4c3ba908de47bade0570cccc599e1c64
Berto Booijink <berto.booijink@nelen-schuurmans.nl>
2012-10-17 16:06:46 +0200

    Distinguish different series (by index, for now)

--- tslib/writers/cassandra_writer.py
+++ tslib/writers/cassandra_writer.py
@@ -15,15 +15,17 @@ class CassandraWriter(object):
         self.qs = queue_size
 
     def write(self, dataframes):
-        sensor_id = 'pixml-test'
-        key_format = sensor_id + ':%Y-%m'
+        key_format = '%Y-%m'
         colname_format = '%Y-%m-%dT%H:%M:%SZ'
+        i = 0
         with self.cf.batch(queue_size=self.qs) as b:
             for df in dataframes:
+                sensor_id = 'pixml-test-%d' % i
                 for timestamp, row in df.iterrows():
                     ts_int = timestamp.astimezone(INTERNAL_TIMEZONE)
                     b.insert(
-                        ts_int.strftime(key_format),
+                        ts_int.strftime(sensor_id + ':' + key_format),
                         dict(("%s_%s" % (ts_int.strftime(colname_format), k),
                               str(v)) for k, v in row.to_dict().iteritems())
                     )
+                i += 1
